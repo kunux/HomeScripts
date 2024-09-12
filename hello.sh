adeb0190d60a77e9a5d978133a14f9feee056938
kunux <blackboba@gmail.com>
2024-09-12 11:32:12 +0200

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -2,6 +2,6 @@
 
 echo "ldv,dml,mlv,sml,lmz,sml"
 echo "Hello, test from git project !"
-read -s "What is your name? " name
+read "What is your name? " name
 	echo "Hello, ${name}!"
 	echo "executed from githidddddddub project......"

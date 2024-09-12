56f156d2132243203c49693e14da2edf72e769ee
kunux <blackboba@gmail.com>
2024-09-12 11:26:06 +0200

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -3,4 +3,4 @@
 echo "Hello, test from git project !"
 read -s "What is your name? " name
 	echo "Hello, ${name}!"
-	echo "executed from githiub project......"
+	echo "executed from githidddddddub project......"

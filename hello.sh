134d6bef4fcb76bc78722f544aac32b1e0ac5cf9
kunux <blackboba@gmail.com>
2024-09-12 12:02:06 +0200

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -10,24 +10,25 @@ echo -e ""
 
 #_____ functions  _______________________________________________________
 CONFIRM() {
-    echo -n "Do you want to ontinue? y or n? "
-    read REPLY
-    case $REPLY in
-    [Yy]) echo 'yup y' ;; # you can change what you do here for instance
-    [Nn]) echo 'nope n' ;;
-    # Here are a few optional options to choose between
-    # 1. Repeat the question
-    *) confirm ;;
-
-    # 2. ignore
-    # *) ;;
-
-    # 3. Exit terminal
-    # *) exit ;;
-
-    esac
-    # REPLY=''
-}
+        echo -n "Do you want to ontinue? y or n? "
+        read REPLY
+        case $REPLY in
+        [Yy]) echo 'yup y' ;; # you can change what you do here for instance
+        [Nn]) echo 'nope n' ;;
+        # Here are a few optional options to choose between
+        # 1. Repeat the question
+        *) CONFIRM ;;
+        esac
+        # REPLY=''
+    }
 
 #_____ Main Script execution  _______________________________________________________
-confirm
+CONFIRM
+print_section_NC "Fetched Variables for $1 from: ... init.sh \& KEYCHAIN ..."
+        if [ $MOUNT=true ]; then
+            echo "MOUNT:${MOUNT} is the value for USER:${ACCOUNT} and SERVER:${SERVER}"
+            GREY_B "PASSWORD:${PASS2}"
+            echo "mounting the share by calling function"
+            else
+               error "mount is false ==  $MOUNT"
+        fi

b4ce22c1db0929e33a81bbd0888a6cb891ad91c2
kunux <blackboba@gmail.com>
2024-09-12 11:40:19 +0200

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -5,3 +5,26 @@ echo "Hello, test from git project !"
 read -pr "What is your name?" NAM
 	echo "Hello, ${NAM}!"
 	echo "executed from githidddddddub project......"
+
+
+ confirm() {
+    echo -n "Continue? y or n? "
+    read REPLY
+    case $REPLY in
+    [Yy]) echo 'yup y' ;; # you can change what you do here for instance
+    [Nn]) echo 'nope n' ;;
+    # Here are a few optional options to choose between
+    # Any other answer:
+
+    # 1. Repeat the question
+    *) confirm ;;
+
+    # 2. ignore
+    # *) ;;
+
+    # 3. Exit terminal
+    # *) exit ;;
+
+    esac
+    # REPLY=''
+}
